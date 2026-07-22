85bc7d2a910abcd66b20983d99ea8364c4e47b38
shrawa26 <dharanij0414@gmail.com>
2026-07-22 22:35:55 +0530

    Day 2 - HTML pages completed

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World! Welcome to Codomax Full Stack Internship.");
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
 });
